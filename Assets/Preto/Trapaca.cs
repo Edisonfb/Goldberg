a9ddf238f615387b7f705c85ea06960a499070bf
Edisonfb <edison.fbnts@gmail.com>
2019-11-07 20:40:59 -0300

    mapa sem joints

---
+++ Assets/Preto/Trapaca.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapaca : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+       // if(collision.GetContact(0))
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Turbo1")
+            rb.velocity += new Vector3(0.2f, 0.0f, 0.0f);
+    }
+}
